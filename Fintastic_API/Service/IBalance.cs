3db09bbfa27c3cbc37742904052769aa5d63cee1
Stephanyy <santosstephany400@gmail.com>
2024-05-11 16:12:24 -0300

    feat: Added IBalance interface and BalanceService implementation

---
+++ Fintastic_API/Service/IBalance.cs
@@ -0,0 +1,10 @@
+﻿namespace Fintastic_API.Service
+{
+    public interface IBalance<T>
+    {
+        Task<decimal> CalculateTotalIncome();
+        Task<decimal> CalculateTotalSpent();
+        Task<decimal> CalculateTotal();
+
+    }
+}
