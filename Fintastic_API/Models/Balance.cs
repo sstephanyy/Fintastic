b2d0dfac1f2eaf522f487c25a680ab843f31b7b2
Stephanyy <santosstephany400@gmail.com>
2024-05-11 13:58:32 -0300

    create the Balance table in DB

---
+++ Fintastic_API/Models/Balance.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fintastic_API.Models
+{
+    public class Balance
+    {
+        public int BalanceId { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Total { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")] // Define o tipo decimal com 18 dígitos no total e 2 dígitos após a vírgula
+        public decimal TotalIncome { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")] 
+        public decimal TotalSpent { get; set; }
+    }
+}
