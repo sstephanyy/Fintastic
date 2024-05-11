5fd5fb3e83e7790e01045ef43dea14d50a10b0b8
Stephanyy <santosstephany400@gmail.com>
2024-05-11 11:08:13 -0300

    Refactor: Improve Category implementation and introduce service

--- Fintastic_API/Program.cs
+++ Fintastic_API/Program.cs
@@ -30,58 +30,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// ==================> CATEGORY CRUD <=====================
-app.MapGet("/categoria", async (DataContext context) =>
-    await context.Categories.ToListAsync()
-).WithTags("Categoria");
 
-
-app.MapGet("/categoria/{id}", async (int id, DataContext context) =>
-{
-    var category = await context.Categories.FindAsync(id);
-    if (category == null)
-    {
-        return Results.NotFound(new { message = "Categoria não encontrada." });
-    }
-    return Results.Ok(category);
-}).WithTags("Categoria");
-
-
-app.MapPost("/categoria", async (Category newCategory, DataContext context) =>
-{
-    context.Categories.Add(newCategory);
-    await context.SaveChangesAsync();
-    return Results.Created($"/categoria/{newCategory.CategoryId}", newCategory);
-}).WithTags("Categoria");
-
-app.MapPut("/categoria/{id}", async (int id, DataContext context, Category updatedCategory) =>
-{
-    var categoryBanco = await context.Categories.AsNoTracking<Category>().FirstOrDefaultAsync(f => f.CategoryId == id); 
-    if (categoryBanco == null) return Results.NotFound();
-
-    context.Categories.Update(updatedCategory);
-    var result = await context.SaveChangesAsync();
-
-    return result > 0
-        ? Results.NoContent()
-        : Results.BadRequest(new { message = "Não foi possível atualizar o registro." });
-}).WithTags("Categoria");
-
-
-app.MapDelete("/categoria/{id}", async (int id, DataContext context) =>
-{
-    var category = await context.Categories.FindAsync(id);
-    if (category == null) return Results.NotFound();
-
-    context.Categories.Remove(category);
-    var result = await context.SaveChangesAsync();
-
-    return result > 0
-        ? Results.NoContent()
-        : Results.BadRequest("Não foi possível deletar o registro.");
-}).WithTags("Categoria");
-
-
+app.MapCategoryRoutes();
 app.MapIncomeRoutes();
 app.MapSpentRoutes();
 
---
+++ Fintastic_API/Routes/CategoryRoute.cs
@@ -0,0 +1,45 @@
+﻿using Fintastic_API.Models;
+using Fintastic_API.Services;
+
+namespace Fintastic_API.Routes
+{
+    public static class CategoryRoute
+    {
+        public static void MapCategoryRoutes(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapGet("/categoria", async (IService<Category> _categoryService) =>
+            {
+                var categoryList = await _categoryService.GetRegisters();
+                return Results.Ok(categoryList);
+            }).WithTags("Categoria").WithName("GetCategories");
+
+            endpoints.MapGet("/categoria/{id}", async (IService<Category> _categoryService, int id) =>
+            {
+                var category = await _categoryService.GetRegisterById(id);
+                if (category != null) return Results.Ok(category);
+                return Results.NotFound();
+            }).WithTags("Categoria").WithName("AddCategoryById");
+
+            endpoints.MapPost("/categoria", async (Category category, IService<Category> categoryService) =>
+            {
+                if (category == null) TypedResults.BadRequest();
+                await categoryService.AddRegister(category);
+                return Results.Created($"{category.CategoryId}", category);
+            }).WithTags("Categoria").WithName("AddCategories");
+
+            endpoints.MapPut("/categoria/{id}", async (IService<Category> _categoryService, int id, Category category) =>
+            {
+                var updatedCategory = await _categoryService.UpdateRegister(id, category);
+                if (updatedCategory != null) return Results.Ok(updatedCategory);
+                return Results.NotFound();
+            }).WithTags("Categoria").WithName("UpdateCategory");
+
+            endpoints.MapDelete("/categoria/{id}", async (IService<Category> _categoryService, int id) =>
+            {
+                var deleteResult = await _categoryService.DeleteRegister(id);
+                if (deleteResult) return Results.Ok();
+                return Results.NotFound();
+            }).WithTags("Categoria").WithName("DeleteCategory");
+        }
+    }
+}
